52244b293fbeda47438fa8926fd684ce4f1ae8df
Maxim <maxl08@list.ru>
2023-07-12 15:56:29 +0300

    Add task

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,7 +1,10 @@
-﻿int GetInfo ()
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int GetInfo ()
 {
-//    int i = 1;
-//    Console.Write("Введите {coordinateNumber} координату {numderPoint} точки: ");
     int inputNumber = Convert.ToInt32(Console.ReadLine());
     return inputNumber;
 }
